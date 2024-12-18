0d21b5317b2822bcd5576d6109e1850daa64afcf
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-12-18 11:29:16 -0300

    add header and todo components

---
+++ src/app/services/todo-signals.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, signal } from '@angular/core';
+import { Todo } from '../models/model/todo.model';
+import { todoKeyLocalStorage } from '../models/enum/todoKeyLocalStorage';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoSignalsService {
+  public todosState = signal<Array<Todo>>([]);
+
+  public updateTodos({ id, title, description, done }: Todo): void {
+    if ((title && id && description !== null) || undefined) {
+      this.todosState.update((todos) => {
+        if (todos !== null) {
+          todos.push(new Todo(id, title, description, done));
+        }
+        return [];
+      });
+      this.saveTodosInLocalStorage();
+    }
+  }
+
+  public saveTodosInLocalStorage(): void {
+    const todos = JSON.stringify(this.todosState());
+    todos && localStorage.setItem(todoKeyLocalStorage.TODO_LIST, todos);
+  }
+}
