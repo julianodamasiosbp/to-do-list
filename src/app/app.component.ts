63058e4c5535394fc98f77f32ac29172ce976e3f
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-12-20 14:56:14 -0300

    add map and filter rxjs method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { TodoCardComponent } from './components/todo-card/todo-card.component';
 import { CommonModule } from '@angular/common';
 import { SchoolData, SchoolService } from './services/school.service';
-import { Observable, zip } from 'rxjs';
+import { filter, from, map, Observable, of, zip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -21,11 +21,32 @@ export class AppComponent implements OnInit {
   public students: Array<SchoolData> = [];
   public teachers: Array<SchoolData> = [];
   private zipSchoolResponses$ = zip(this.getStudents$, this.getTeachers$);
-
+  private ages = of(20, 30, 40, 50, 60, 70);
+  private peopleDatas = from([
+    {
+      name: 'Marcos',
+      age: 20,
+      jobDescription: 'Teacher',
+    },
+    {
+      name: 'Maria',
+      age: 25,
+      jobDescription: 'Software Engineer',
+    },
+    {
+      name: 'João',
+      age: 27,
+      jobDescription: 'UX Designer',
+    },
+  ]);
   ngOnInit(): void {
+    this.getPeopleByJobDescription('Software Engineer');
+    this.getMultipliedAges();
+    this.getPeopleJobDescription();
     this.getSchoolDatas();
     this.getStudents$.subscribe({
       next: (response) => {
+        console.log('====================');
         console.log('Students$: ', response);
       },
     });
@@ -34,10 +55,45 @@ export class AppComponent implements OnInit {
   public getSchoolDatas(): void {
     this.zipSchoolResponses$.subscribe({
       next: (response) => {
+        console.log('====================');
         console.log('Students: ', response[0]);
         console.log('Teachers: ', response[1]);
       },
     });
+  }
+
+  public getMultipliedAges(): void {
+    this.ages.pipe(map((age) => age * 2)).subscribe({
+      next: (response) => {
+        console.log('====================');
+        console.log('Multiplied Age: ', response);
+      },
+    });
+  }
+
+  public getPeopleJobDescription(): void {
+    this.peopleDatas.pipe(map((people) => people.jobDescription)).subscribe({
+      next: (response) => {
+        console.log('====================');
+        console.log('Job Description: ', response);
+      },
+    });
+  }
+
+  public getPeopleByJobDescription(value: string): void {
+    this.peopleDatas
+      .pipe(
+        filter((people) => people.jobDescription === value),
+        map((people) => people.name)
+      )
+      .subscribe({
+        next: (response) => {
+          console.log('====================');
+          console.log(
+            `${response} is the Person with the Job Description: ${value}`
+          );
+        },
+      });
   }
 
   private getStudentsDatas(): Observable<Array<SchoolData>> {
